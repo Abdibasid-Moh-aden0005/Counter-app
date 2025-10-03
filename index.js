8b5e06567b1da9b86d9dee17f4bb7e57bb104238
Abdibasid-Moh-aden0005 <abdibasid864@gmail.com>
2025-10-03 21:07:34 +0300

    changes made

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
+let parent = document.querySelector("#parent")
+let addCard = document.querySelectorAll("button")[0]
+console.log(addCard)
 
-let score = document.querySelector("#score")
-let pastScore = document.querySelector("#past")
-let addButton = document.querySelectorAll("button")[0]
-let saveButton = document.querySelectorAll("button")[1]
-let closeButton = document.querySelectorAll("button")[2]
 
-let counter = 0;
+addCard.addEventListener("click",()=> addFun())
+
+function addFun () {
+    let holder = document.createElement("div")
+    holder.setAttribute("class","mx-2")
+    holder.innerHTML = `
+     
+    <!-- card -->
+      <div
+        class="p-2 shadow-lg shadow-gray-800/50 rounded ring ring-gray-300 transtion duration-700 ease-in-out hover:translate-y-1 hover:scale-105"
+      >
+        <div class="my-3">
+          <h2 class="text-center text-xl font-bold">Counter App</h2>
+        </div>
+        <!-- Body -->
+        <div class="">
+          <p class="font-[700] text-2xl text-black text-start" id="score">
+            Score:
+          </p>
+          <div class="mt-[4rem] px-3 flex space-x-4">
+            <button
+              class="bg-indigo-500 text-white font-[700] text-xl rounded-xl px-3 py-2 hover:bg-indigo-800"
+              id="ad"
+            >
+              Add
+            </button>
+            <button
+              class="bg-indigo-500 text-white font-[700] text-xl rounded-xl px-3 py-2 hover:bg-indigo-800"
+              id="save"
+            >
+              Save
+            </button>
+            <button
+              class="bg-indigo-500 text-white font-[700] text-xl rounded-xl px-3 py-2 hover:bg-indigo-800"
+              id="close"
+            >
+              Close
+            </button>
+          </div>
+          <!-- output: -->
+          <div class="py-4 mt-3">
+            <p class="text-start capitalize font-[500]" id="past">
+              past scores :
+            </p>
+          </div>
+        </div>
+      </div>
+    `
+  parent.appendChild(holder)
+  
+    // 
+    let score = holder.querySelector("#score")
+    let pastScore = holder.querySelector("#past")
+    let addButton = holder.querySelector("#ad")
+    let saveButton = holder.querySelector("#save")
+  let closeButton = holder.querySelector("#close")
+  console.log(addButton)
+    let counter = 0;
 
 addButton.addEventListener("click", () => {
     counter += 1;
-    score.textContent = `Score: ${counter}`
+  score.textContent = `Score: ${counter}`
 })
 
 saveButton.addEventListener("click", function () {
@@ -22,6 +77,9 @@ closeButton.addEventListener("click", () => {
     counter = 0;
      score.textContent = `Score:`
 })
+  
+   
+}
 
 // let firstName = "abdibasid"
 // let score = 10.4;
